d6fa93e2dbc4ba501b9aae7745da4c68f5e65ff9
Jonas Birmé <jonas.birme@eyevinn.se>
2025-02-21 18:41:58 +0100

    feat: basic auth adapter

---
+++ README.md
@@ -0,0 +1,26 @@
+# Basic Auth Adapter
+
+Deploy as a [Web Runner](https://docs.osaas.io/osaas.wiki/Service%3A-Web-Runner.html) to provide Basic Auth authentication for one of your Eyevinn Open Source Cloud service instances.
+
+## Usage
+
+Specify service id, username and password with the environment variables `SERVICE_ID`, `USERNAME` and `PASSWORD`. See the section "Configuring your application" in the [Web Runner documentation](https://docs.osaas.io/osaas.wiki/Service%3A-Web-Runner.html) for instructions how to provide these environment variables.
+
+Create a Web Runner instance from this GitHub repository or make a fork and adapt it to the way you like.
+
+### Create a Web Runner from this repository
+
+Generate a personal GitHub token that have access to this repository on behalf of your user. We recommend you restrict this token to only access what it needs. Store this GitHub token as a secret in the Web Runner service.
+
+Store your Eyevinn Open Source Cloud personal access token (found in Settings/API in web console) as a secret in the Web Runner service.
+
+Assuming that the GitHub token secret is stored as `ghtoken`, OSC access token as `osctoken` and the name of the configuration service is `authadaptconfig` you can deploy the runner using the OSC command line tool.
+
+```bash
+% osc create eyevinn-web-runner myadapter \
+  -o GitHubUrl=https://github.com/EyevinnOSC/basic-auth-adapter
+  -o GitHubToken="{{secrets.ghtoken}}" \
+  -o OscAccessToken="{{secrets.osctoken}}" \
+  -o ConfigService=authadaptconfig
+```
+
---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import { Context, getInstance } from "@osaas/client-core";
+import fastify from "fastify";
+import basicAuth from '@fastify/basic-auth';
+
+const SERVICE_ID = process.env.SERVICE_ID || 'eyevinn-hls-monitor';
+const USERNAME = process.env.USERNAME || 'myuser';
+const PASSWORD = process.env.PASSWORD || 'changeme';
+
+let lastRefreshedTs: number | undefined;
+
+async function refreshToken(ctx: Context): Promise<string> {
+  const token = await ctx.getServiceAccessToken(SERVICE_ID);
+  lastRefreshedTs = Date.now();
+  return token;
+}
+
+async function main() {
+  const ctx = new Context();
+  const server = fastify();
+  const authenticate = { realm: 'Restricted Area' };
+  const validate = async (username: string, password: string) => {
+    if(username === USERNAME && password === PASSWORD) {
+      return;
+    }
+    throw new Error('Unauthorized');
+  };
+  server.register(basicAuth, { validate, authenticate });
+  server.after(() => {
+    server.addHook('onRequest', server.basicAuth);
+    let token: string | undefined = undefined;
+  
+    server.get('/', async (request, reply) => {
+      reply.send('Hello World');
+    });
+
+    server.get<{
+      Params: { instanceName: string };
+    }>('/:instanceName/*', async (request, reply) => {
+      try {
+        const upstreamPath = request.originalUrl.replace(`/${request.params.instanceName}`, '');
+        const tokenExpired = lastRefreshedTs && Date.now() - lastRefreshedTs > 3600 * 1000;
+        const instaneName = request.params.instanceName;
+        if (!token || tokenExpired) {
+          token = await refreshToken(ctx);
+        }
+        const instance = await getInstance(ctx, SERVICE_ID, instaneName, token);
+        const res = await fetch(new URL(upstreamPath, instance.url), {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        if (res.headers.get('content-type') === 'application/json') {
+          const data = await res.json();
+          reply.code(res.status).send(data);
+        } else {
+          const data = await res.text();
+          reply.code(res.status).send(data);
+        }
+      } catch (err) {
+        reply.code(500).send(err);
+      }
+    });
+  });
+
+  server.listen({ host: '0.0.0.0', port: process.env.PORT ? Number(process.env.PORT) : 8080 }, (err, address) => {
+    if (err) console.error(err);
+    console.log(`Server listening at ${address}`);
+  });
+}
+
+main();
